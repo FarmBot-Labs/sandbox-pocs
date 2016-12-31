dd7ec3dcb13b9fcf92949096bfa11ad9a1e8cb24
Rick Carlino <rick.carlino@gmail.com>
2016-12-30 22:22:59 -0600

    Clean up indentation of emiter output.

--- emitter.rb
+++ emitter.rb
@@ -2,21 +2,29 @@ require "pry"
 require "json"
 KEYS = (KIND, ARGS, BODY, COMMENTS = "kind", "args", "body", "comments")
 REQUIRED_KEYS = [ARGS, KIND]
+$indent = 0
 tree = JSON.parse(File.read("./ast.json"))
 
 def is_node?(maybe)
-  (maybe.is_a?(Hash) &&
-  (REQUIRED_KEYS & maybe.keys) == REQUIRED_KEYS)
+  (maybe.is_a?(Hash) && (REQUIRED_KEYS & maybe.keys) == REQUIRED_KEYS)
 end
 
 def go(thing)
   if is_node?(thing)
-    kind = thing[KIND] || "panic"
-    body = thing[BODY]
-    args = thing[ARGS].map { |p| "#{p[0]}: #{go(p[1])}" }.join(", ")
-    head = "F.#{ kind }(#{ args })"
-    tail = body ? " { #{body.map{|b| go(b)}.join("\n")} } " : ""
-    return head + tail
+    kind   = thing[KIND] || "panic"
+    body   = thing[BODY]
+    args   = thing[ARGS].map { |p| "#{p[0]}: #{go(p[1]).strip}" }.join(", ")
+    head   = "F.#{ kind }(#{ args })"
+    indent = "  " * $indent
+    if body
+      $indent += 1
+      content = body.map{|b| go(b)}.join("\n")
+      tail = " do #{"\n" + indent + content + "\n" }#{indent}end"
+      $indent -= 1
+    else
+      tail = ""
+    end
+    return indent + head + tail
   else
     JSON.dump(thing)
   end
